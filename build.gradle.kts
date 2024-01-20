0dd1c3f9ef032af293a8cec2fb9100aa5c464a64
Martin Vysny <mavi@vaadin.com>
2024-01-20 10:06:01 +0200

    1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.vaadin-simple-security"
-    version = "1.0-SNAPSHOT"
+    version = "1.0"
 
     repositories {
         mavenCentral()
