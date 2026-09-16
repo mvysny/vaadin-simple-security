8877b777adbb12e7c5cbd88a0bc2937b41af9f8a
Martin Vysny <martin@vysny.me>
2026-09-16 11:25:30 +0300

    2.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ tasks.check { dependsOn(verifyDesignTripwires) }
 
 allprojects {
     group = "com.github.mvysny.vaadin-simple-security"
-    version = "2.0"
+    version = "2.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
